ce7024dffc0521a65f11cac1e46bce6069444ce3
JossePerez <jose.perez18@tectijuana.edu.mx>
2023-08-21 23:56:55 -0700

    Creacion del modulo principal

--- Header/header.js
+++ Header/header.js
@@ -9,3 +9,5 @@ window.addEventListener("scroll", function() {
     header.classList.remove("scrolled");
   }
 });
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
